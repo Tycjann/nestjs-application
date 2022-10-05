e42d773d40a7e1f506e8e6c41f5126ea268814e0
Mirosław Gucwa <mgucwa@gmail.com>
2022-10-05 23:05:32 +0200

    Add: Subscriber

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,17 +7,20 @@ export const AppDataSource = new DataSource({
   username: 'usr_kodilla-nest-js-api',
   password: 'L7BABOCEBI',
   database: 'kodilla-nest-js-api',
-  // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  entities: ['dist/**/*.entity.js'],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  // entities: ['dist/**/*.entity.js'],
   logging: true,
   // synchronize - zapewnia nam aktualizację naszej tabeli podczas zmian w schemacie
   // W przypadku uruchamiania aplikacji na produkcji ta opcja powinna być ustawiona na false
+  // ? przy true migracje nie zapisują się w tabeli history
+  //  ? + pytanie o tablice migration - dlaczego jest pysta
   synchronize: true,
   // synchronize: false,
   // dropSchema: false,
   // migrationsRun: true,
   migrationsRun: false,
-  // migrations: [__dirname + '/db/migrations/**/*{.ts,.js}'],
-  migrations: ['dist/**/migrations/*.js'],
+  migrations: [__dirname + '/db/migrations/**/*{.ts,.js}'],
+  // migrations: ['dist/**/migrations/*.js'],
   migrationsTableName: 'history',
+  subscribers: [__dirname + '/db/subscribers/**/*{.ts,.js}'],
 });
