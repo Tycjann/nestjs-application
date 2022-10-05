bbca04512a54094a21a39334b96678eae0cf2e91
Mirosław Gucwa <mgucwa@gmail.com>
2022-10-05 22:00:52 +0200

    Fix: entityrepository - pordukt, user address

--- src/products/db/tags.repository.ts
+++ src/products/db/tags.repository.ts
@@ -1,8 +1,12 @@
-import { EntityRepository, Repository, In } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository, In } from 'typeorm';
 import { Tags } from './tags.entity';
 
-@EntityRepository(Tags)
+@Injectable()
 export class TagRepository extends Repository<Tags> {
+  constructor(private dataSource: DataSource) {
+    super(Tags, dataSource.createEntityManager());
+  }
   findTagsByName(names: string[]): Promise<Tags[]> {
     return this.find({
       where: {
--- src/users/db/users.repository.ts
+++ src/users/db/users.repository.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource, EntityRepository, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './users.entity';
-
-// @EntityRepository(User)
-// export class UserRepository extends Repository<User> {}
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
-
   // exampleQueryBuilder() {
   //   return this.dataSource.getRepository(User).createQueryBuilder();
   // }
